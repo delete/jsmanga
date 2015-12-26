b164e3c77517eea71a790ef6817fac27fb3c8f44
delete <pinheiro.llip@gmail.com>
2015-12-25 23:32:56 -0200

    fix identation and add comma first

--- spider.js
+++ spider.js
@@ -8,103 +8,102 @@ var IMAGE_URL = 'http://mangas2014.centraldemangas.com.br/one_piece/one_piece'
 
 //These headers are used just when is downloading an image.
 var HEADER_ONE_PIECE = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 \
-                   Firefox/42.0',
-    'Host': 'mangas2014.centraldemangas.com.br',
-    'Accept': 'image/png,image/*;q=0.8,*/*;q=0.5',
-    'Accept-Language': 'pt-BR,en;q=0.5',
-    'Referer': 'http://mangaop.info/capitulos/747'
+   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:42.0) Gecko/20100101 \
+       Firefox/42.0'
+  ,'Host': 'mangas2014.centraldemangas.com.br'
+  ,'Accept': 'image/png,image/*;q=0.8,*/*;q=0.5'
+  ,'Accept-Language': 'pt-BR,en;q=0.5'
+  ,'Referer': 'http://mangaop.info/capitulos/747'
 }
 
 function main () {
-	var args = process.argv.slice(2);
-	download_chapter(args[0]);
+  var args = process.argv.slice(2);
+  download_chapter(args[0]);
 }
 
 
 function download_chapter (chapter) {
 
-	var url = generate_mainUrl(chapter);
+  var url = generate_mainUrl(chapter);
 
-	var options = {
-		url : url,
-	};
+  var options = {
+    url : url,
+  };
 
-	function callback(error, response, body) {
-		if (!error) {
-			number_of_chapters = get_chapters(body);
+  function callback(error, response, body) {
+    if (!error) {
+      number_of_chapters = get_chapters(body);
 
-			if(!number_of_chapters){ number_of_chapters = 0 }
-			
-			console.log("Número de páginas: " + number_of_chapters);
+      if(!number_of_chapters){ number_of_chapters = 0 }
 
-			for (var i = 1; i <= number_of_chapters; i++) {
-				download_image(chapter, i);
-			};
-		}
-	}
+      console.log("Número de páginas: " + number_of_chapters);
 
-	request(options, callback);
+      for (var i = 1; i <= number_of_chapters; i++) {
+        download_image(chapter, i);
+      };
+    }
+  }
 
+  request(options, callback);
 }
 
 
 function generate_mainUrl (chapter) {
-	// The URL will be: Ex for chapter 800
-	// http://mangaop.info/capitulos/800#1
-	return MAIN_URL + chapter + '#1'
+  // The URL will be: Ex for chapter 800
+  // http://mangaop.info/capitulos/800#1
+  return MAIN_URL + chapter + '#1'
 }
 
 
 function get_chapters (body) {
-	var $ = cheerio.load(body);
+  var $ = cheerio.load(body);
 
-	var chapters = $("#capPages").text().replace("\t", " ").trim().split("\t");
+  var chapters = $("#capPages").text().replace("\t", " ").trim().split("\t");
 
-	// Return the last one.
-	return chapters[chapters.length - 1];
+  // Return the last one.
+  return chapters[chapters.length - 1];
 }
 
 
 function download_image (chapter, number) {
 
-	var imageUrl = generate_imageUrl(chapter, number);
+  var imageUrl = generate_imageUrl(chapter, number);
 
-	var options = {
-		url : imageUrl,
-		headers: HEADER_ONE_PIECE
-	};
+  var options = {
+     url : imageUrl
+    ,headers: HEADER_ONE_PIECE
+  };
 
-	var filename = 'capitulo-' + number + '.jpg';
+  var filename = 'capitulo-' + number + '.jpg';
 
-	var dir = create_folder(chapter)
-	var path_to_save = dir + filename
+  var dir = create_folder(chapter);
+  var path_to_save = dir + filename;
 
-	request(options).pipe(fs.createWriteStream(path_to_save)).on('close', callback);
+  function callback () {
+    console.log('Arquivo salvo ' + filename)
+  }
 
-	function callback () {
-		console.log('Arquivo salvo ' + filename)
-	}
+  request(options).pipe(fs.createWriteStream(path_to_save)).on('close', callback);
 }
 
 
 function generate_imageUrl (chapter, number) {
-	// The URL will be: Ex for chapter 800 and number 1
-	// http://mangas2014.centraldemangas.com.br/one_piece/one_piece800-01.jpg
-	if (number < 10) {
-		number = '0' + number
-	}
-	return IMAGE_URL + chapter + '-' + number + '.jpg' 
+  // The URL will be: Ex for chapter 800 and number 1
+  // http://mangas2014.centraldemangas.com.br/one_piece/one_piece800-01.jpg
+  if (number < 10) {
+    number = '0' + number
+  }
+  return IMAGE_URL + chapter + '-' + number + '.jpg' 
 }
 
 
 function create_folder(chapter) {
-	var dir = 'capitulo-' + chapter;
+  var dir = 'capitulo-' + chapter;
 
-	if (!fs.existsSync(dir)){
-	    fs.mkdirSync(dir);
-	}
-	return dir + '/'
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+    return dir + '/'
 }
 
 
